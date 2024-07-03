ae2c8dd48e905ad4987b18734619b8935f46e922
jmartinez419 <justynmartinez419@gmail.com>
2024-07-03 09:10:03 -0400

    followed all examples for the for loops

---
+++ src/Example1.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Example1 {
+    public static void main(String[] args) {
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+}
